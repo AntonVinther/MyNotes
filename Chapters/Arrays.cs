914775422decf76b5087f06ee99995f7896f2535
Anton Vinther <anto3673@mail.hansenberg.dk>
2023-09-06 12:35:39 +0200

    arrays tilføjet

---
+++ Chapters/Arrays.cs
@@ -0,0 +1,23 @@
+using System.Reflection.Metadata.Ecma335;
+
+public static class Arrays
+{
+public static string side1 =
+@"Arrays i C# er datastrukturer,
+der gemmer en samling af elementer af samme datatype under ét navn.
+De har en fast størrelse og bruges til at organisere data i programmer.";
+
+public static string side2 =
+@"For at forklare næmrere:
+Deklaration: Du deklarerer et array ved at angive dets datatype og navn, f.eks. int[] tal;
+
+Initialisering: Arrays skal initialiseres med en størrelse og elementer,
+f.eks. int[] tal = new int[5]; initialiserer et heltalsarray med 5 pladser.
+
+Tilgængelighed: Elementer i et array kan tilgås ved hjælp af indekser,
+f.eks. tal[0] for det første element.
+
+Størrelse: Arrays har en fast størrelse, der defineres ved initialiseringen og kan ikke ændres.
+
+Looping: Ofte bruges løkker som for til at gennemgå og arbejde med array-elementer.";
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,48 @@
-﻿List<Page> pages = new List<Page>();
+﻿Chapter chapter1 = new Chapter("Kapitel 1");
+{
+    List<Page> pages = new List<Page>();
 
-Page side = new Page(Variabler.side1);
-Page side2 = new Page(Variabler.side2);
- 
-pages.Add(side);
-pages.Add(side2);
+    Page side = new Page(Variabler.side1);
+    Page side2 = new Page(Variabler.side2);
 
-Chapter chapter1 = new Chapter("Kapitel 1");
-chapter1.Pages = pages;
-Chapter chapter2 = new Chapter("Kapitel 2", pages);
+    pages.Add(side);
+    pages.Add(side2);
+    
+    chapter1.Pages = pages;
+}
 
-Chapter chapter3 = new Chapter("Kapitel 3", pages);
+
+Chapter chapter2 = new Chapter("Kapitel 2");
+{
+    List<Page> pages = new List<Page>();
+
+    Page side = new Page(conditionals.side1);
+    Page side2 = new Page(conditionals.side2);
+
+    pages.Add(side);
+    pages.Add(side2);
+    
+    chapter2.Pages = pages;
+}
+
+Chapter chapter3 = new Chapter("Kapitel 3");
+{
+    List<Page> pages = new List<Page>();
+
+    Page side = new Page(Arrays.side1);
+    Page side2 = new Page(Arrays.side2);
+
+    pages.Add(side);
+    pages.Add(side2);
+    
+    chapter3.Pages = pages;
+}
 
 Book bog1 = new Book("Dine Programmeringsnoter");
 List<Chapter> chapters = new List<Chapter>();
 chapters.Add(chapter1);
 chapters.Add(chapter2);
-
+chapters.Add(chapter3);
 bog1.Chapters = chapters;
 
 bog1.Start();
