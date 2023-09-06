b4387da8abb4bd8f3de11ac2b62e22767ef1d546
Anton Vinther <anto3673@mail.hansenberg.dk>
2023-09-06 11:20:04 +0200

    Lavet kapitel vairabler

---
+++ Chapters/variabler.cs
@@ -0,0 +1,27 @@
+public static class Variabler
+{
+    public static string side1 =
+    @"
+    En variabel i C# er en navngiven beholder til opbevaring af data.
+
+Du deklarerer en variabel ved at angive dens datatyper og navn, f.eks. int alder;.
+
+Du kan initialisere en variabel samtidig med dens deklaration, f.eks. int alder = 30;.
+
+C# har forskellige datatyper som int, double, string, og bool til at repræsentere forskellige typer data.
+
+Variabler har et gyldighedsområde (scope) og skal have unikke navne.
+
+Konstanter er værdier, der ikke kan ændres efter initialisering, deklareres med ""const"" nøgleordet.
+
+Variabler er afgørende for at gemme og arbejde med data i C#-programmer.";
+
+public static string side2 =
+@"
+Mere specifikt:
+int: Heltal.
+float (eller double): Decimaltal.
+string: Tekst.
+bool: Sandt/falsk.";
+
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿List<Page> pages = new List<Page>();
 
-Page side = new Page(MyNotes.side);
-Page side2 = new Page(MyNotes.side2);
+Page side = new Page(Variabler.side1);
+Page side2 = new Page(Variabler.side2);
  
 pages.Add(side);
 pages.Add(side2);
@@ -9,6 +9,8 @@ pages.Add(side2);
 Chapter chapter1 = new Chapter("Kapitel 1");
 chapter1.Pages = pages;
 Chapter chapter2 = new Chapter("Kapitel 2", pages);
+
+Chapter chapter3 = new Chapter("Kapitel 3", pages);
 
 Book bog1 = new Book("Dine Programmeringsnoter");
 List<Chapter> chapters = new List<Chapter>();
